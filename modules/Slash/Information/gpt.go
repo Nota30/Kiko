ef447c6705a972b0b30154638cbd64eeac229449
Falak Tulsi <falaktulsi@gmail.com>
2023-02-24 13:26:21 -0800

    Added Moderation Code for GPT Command

--- modules/Slash/Information/gpt.go
+++ modules/Slash/Information/gpt.go
@@ -2,7 +2,10 @@ package modules
 
 import (
 	"context"
+	"encoding/json"
 	"fmt"
+	"log"
+	"net/http"
 	"strings"
 	"time"
 
@@ -12,7 +15,32 @@ import (
 	"github.com/bwmarrin/discordgo"
 )
 
+type record struct {
+	Word     string
+	Language string
+}
+
+type profanityStruct struct {
+	Records []record
+}
+
 func GPT(s *discordgo.Session, m *discordgo.MessageCreate) {
+	resp, err := http.Get("https://raw.githubusercontent.com/turalus/encycloDB/master/Dirty%20Words/DirtyWords.json")
+	if err != nil {
+		log.Fatalln(err)
+	}
+	//We Read the response body on the line below.
+
+	defer resp.Body.Close()
+
+	//Create a variable of the same type as our model
+	pResp := &profanityStruct{}
+	//Decode the data
+	if err = json.NewDecoder(resp.Body).Decode(&pResp); err != nil {
+		log.Fatal(err)
+	}
+
+	//Convert the body to type string
 	client := gpt3.NewClient(tools.GetEnv("OpenAIApiKey"))
 
 	goCtx, cancel := context.WithTimeout(context.Background(), 30*time.Second)
@@ -26,10 +54,15 @@ func GPT(s *discordgo.Session, m *discordgo.MessageCreate) {
 	embed := &discordgo.MessageEmbed{}
 	embed.Title = "Chat GPT"
 	embed.Color = 42622
-	embed.Description = response
 
 	embed.Thumbnail = &discordgo.MessageEmbedThumbnail{}
 	embed.Thumbnail.URL = "https://preview.redd.it/what-does-the-chatgpt-symbol-mean-does-it-have-meaning-like-v0-ogw5okm1bz5a1.jpg?auto=webp&s=51f427fda04a94889e4abfcbc6e6848b77be81ba"
+
+	if success, errMsg := moderationTest(response, pResp); success {
+		embed.Description = response
+	} else {
+		embed.Description = errMsg
+	}
 
 	_, err = s.ChannelMessageSendEmbed(m.ChannelID, embed)
 
@@ -66,4 +99,22 @@ func getResponse(ctx context.Context, client gpt3.Client, question string) (resp
 	response = strings.TrimLeft(response, "\n")
 
 	return response, nil
+}
+
+func moderationTest(str string, pResp *profanityStruct) (bool, string) {
+	for _, word := range strings.Split(str, " ") {
+		if word[0] == '@' {
+			return false, "Whoa there buddy! Let's not do a ping okay?"
+		}
+		for _, record := range pResp.Records {
+			for _, part := range strings.Split(record.Word, " ") {
+				if strings.ToLower(word) == part {
+					return false, "Whoa there buddy! Let's not say bad words"
+				}
+			}
+		}
+	}
+
+	return true, ""
+
 }
