489ca04f9507d845b8a495efa7bc68d55ddd2e0e
Falak Tulsi <falaktulsi@gmail.com>
2023-04-18 18:20:44 -0700

    Swords toml file,  random weapon selector

--- modules/Information/register.go
+++ modules/Information/register.go
@@ -1,6 +1,8 @@
 package modules
 
 import (
+	"math/rand"
+	"strconv"
 	"time"
 	"unicode"
 
@@ -28,19 +30,18 @@ func Register(s *discordgo.Session, i *discordgo.InteractionCreate) {
 	embed := &discordgo.MessageEmbed{
 		Color: config.Color.Default,
 		Author: &discordgo.MessageEmbedAuthor{
-			Name: "Kiko Registration",
+			Name:    "Kiko Registration",
 			IconURL: i.Member.AvatarURL(""),
 		},
-		Description: 
-		"Welcome to Kiko! Here you will register into the game and choose a class." +
-		"It is also recommended to check out the `/tutorial` command before you proceed with the game.\n" +
-		"**Please note that you cannot change classes further on in the game.**\n\n" + 
-		"> There are 5 classes, which are `Warrior`, `Mage`, `Archer`, `Assassin` and `Martial Artist`\n\n" +
-		"Each class has several sub-classes which you will evolve into as you further progress into the game. " + 
-		"You will start out with the base class and recieve the default weapon for that class. " +
-		"For more information on classes please check `/classes`.\n",
+		Description: "Welcome to Kiko! Here you will register into the game and choose a class." +
+			"It is also recommended to check out the `/tutorial` command before you proceed with the game.\n" +
+			"**Please note that you cannot change classes further on in the game.**\n\n" +
+			"> There are 5 classes, which are `Warrior`, `Mage`, `Archer`, `Assassin` and `Martial Artist`\n\n" +
+			"Each class has several sub-classes which you will evolve into as you further progress into the game. " +
+			"You will start out with the base class and recieve the default weapon for that class. " +
+			"For more information on classes please check `/classes`.\n",
 		Footer: &discordgo.MessageEmbedFooter{
-			Text: "Kiko is a QT",
+			Text:    "Kiko is a QT",
 			IconURL: s.State.User.AvatarURL(""),
 		},
 		Timestamp: time.Now().Format(time.RFC3339),
@@ -54,9 +55,9 @@ func Register(s *discordgo.Session, i *discordgo.InteractionCreate) {
 				discordgo.ActionsRow{
 					Components: []discordgo.MessageComponent{
 						discordgo.SelectMenu{
-							CustomID: "select_class",
+							CustomID:    "select_class",
 							Placeholder: "Choose your class 👇",
-							Options: choices,
+							Options:     choices,
 						},
 					},
 				},
@@ -71,10 +72,10 @@ func Register(s *discordgo.Session, i *discordgo.InteractionCreate) {
 				discordgo.ActionsRow{
 					Components: []discordgo.MessageComponent{
 						discordgo.SelectMenu{
-							CustomID: "select_class",
+							CustomID:    "select_class",
 							Placeholder: "Choose your class 👇",
-							Disabled: true,
-							Options: choices,
+							Disabled:    true,
+							Options:     choices,
 						},
 					},
 				},
@@ -88,22 +89,22 @@ func RegisterSelector(s *discordgo.Session, i *discordgo.InteractionCreate) {
 	// logrus.Info(isExpired.Minutes())
 
 	k := *config.Classes
+	swords := *config.Swords
 
 	embed := &discordgo.MessageEmbed{
 		Author: &discordgo.MessageEmbedAuthor{
-			Name: "Class Selection",
+			Name:    "Class Selection",
 			IconURL: i.Member.AvatarURL(""),
 		},
 		Color: config.Color.Default,
-		Description: 
-			"The `" + i.MessageComponentData().Values[0] + "` class has been selected. " +
+		Description: "The `" + i.MessageComponentData().Values[0] + "` class has been selected. " +
 			"You have now been given the base sub-class of **`" + k[i.MessageComponentData().Values[0]]["1"][0] + "`.**\n" +
+			"Your randomly selected weapon is **`" + swords["common"][strconv.Itoa(rand.Intn(1)+1)].Name + "`**\n" +
 			"I hope you have fun and enjoy your time on Kiko!",
 		Timestamp: time.Now().Format(time.RFC3339),
 		Footer: &discordgo.MessageEmbedFooter{
 			Text: "Kiko's Epic World",
 		},
-		
 	}
 
 	s.InteractionRespond(i.Interaction, &discordgo.InteractionResponse{
